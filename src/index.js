83442fd0d747256d69c054168db524b661887a15
Stella Anastasia <stella@hairylegs.com>
2020-02-16 14:49:12 -0500

    test

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import "./styles.css";
 import NavBar from "./navBar.js";
 import Sales from "./Sales";
 import Rentals from "./Rentals";
-import Agents from "./Agents";
+
 import Press from "./Press";
 import About from "./About";
 import FAQ from "./FAQ";
