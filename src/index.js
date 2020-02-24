e6b3900e8e5030eccd2b5e58e438c6bbf4a8d415
Stella Anastasia <stella@hairylegs.com>
2020-02-23 23:24:32 -0500

    Nav links and spacing adjusted

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./styles.css";
-import Sales from "./Sales";
-import Rentals from "./Rentals";
-import Agents from "./Agents";
-import Press from "./Press";
-import About from "./About";
-import Services from "./Services";
-import Company from "./Company";
-import FAQ from "./FAQ";
-import MarketReport from "./MarketReport";
+
 import Main from "./Home";
 import Example from "./example";
 import Demo from "./demo";
@@ -18,24 +9,7 @@ import Demo from "./demo";
 function App() {
   return (
     <div className="App">
-      <div className="Body">
-        <BrowserRouter>
-          <div className="Top">
-            <Switch>
-              <Route path="./Home" component={Main} />
-              <Route path="/Sales" component={Sales} />
-              <Route path="/Rentals" component={Rentals} />
-              <Route path="/Agents" component={Agents} />
-              <Route path="/Press" component={Press} />
-              <Route path="/About" component={About} />
-              <Route path="/Services" component={Services} />
-              <Route path="/Company" component={Company} />
-              <Route path="/FAQ" component={FAQ} />
-              <Route path="/MarketReport" component={MarketReport} />
-            </Switch>
-          </div>
-        </BrowserRouter>
-      </div>
+      <div className="Body" />
     </div>
   );
 }
